b732f9346f6688aaa2635b8cae74ec249beb6f12
VitoBertucci <vito.rob.bertucci@gmail.com>
2021-09-13 20:33:42 -0400

    v2.1
    Changed project to what prof wanted after project was graded
    -no loop
    -use same variables for both restaurant objects

--- Bertucci-CIS2151-Project_1/src/bertucci/cis2151/project_1/BertucciCIS2151Project_1.java
+++ Bertucci-CIS2151-Project_1/src/bertucci/cis2151/project_1/BertucciCIS2151Project_1.java
@@ -6,48 +6,47 @@ public class BertucciCIS2151Project_1 {
         //create scanner obj
         Scanner kb = new Scanner(System.in);
 
-        //loop for user prompt to run twice and create two different objects
-        int i = 0;
-        while(i < 2) {
+        //prompt user and store responses
+        System.out.println("Enter Name:");
+        String userName = kb.nextLine();
+        System.out.println("Enter FoodType:");
+        String userFoodType = kb.nextLine();
+        System.out.println("Enter URL:");
+        String userURL = kb.nextLine();
 
-            //prompt user and store responses
-            System.out.println("Enter Name:");
-            String userName = kb.nextLine();
-            System.out.println("Enter FoodType:");
-            String userFoodType = kb.nextLine();
-            System.out.println("Enter URL:");
-            String userURL = kb.nextLine();
+        //create restaurant object with default constructor and set attributes with set methods
+        FastFoodAndSnack restaurant = new FastFoodAndSnack();
+        restaurant.setName(userName);
+        restaurant.setFoodType(userFoodType);
+        restaurant.setURL(userURL);
 
-            if (i == 0) {
-                //create restaurant object with default constructor and set attributes with set methods
-                FastFoodAndSnack restaurant = new FastFoodAndSnack();
-                restaurant.setName(userName);
-                restaurant.setFoodType(userFoodType);
-                restaurant.setURL(userURL);
+        //display using the display method
+        System.out.println("----using display method----");
+        restaurant.display();
 
-                //display using the display method
-                System.out.println("----using display method----");
-                restaurant.display();
+        //display using get methods
+        System.out.println("------using get methods-----");
+        System.out.println("Name: " + restaurant.getName());
+        System.out.println("Food Type: " + restaurant.getFoodType());
+        System.out.println("URL: " + restaurant.getURL());
+        System.out.println("---create another object----");
 
-                //display using get methods
-                System.out.println("------using get methods-----");
-                System.out.println("Name: " + restaurant.getName());
-                System.out.println("Food Type: " + restaurant.getFoodType());
-                System.out.println("URL: " + restaurant.getURL());
-                System.out.println("---create another object----");
-            }
+        //prompt user and store again for new restaurant object
+        System.out.println("Enter Name:");
+        userName = kb.nextLine();
+        System.out.println("Enter FoodType:");
+        userFoodType = kb.nextLine();
+        System.out.println("Enter URL:");
+        userURL = kb.nextLine();
 
-            if (i == 1) {
-                //create new second restaurant object using non-default constructor
-                FastFoodAndSnack restaurantAgain = new FastFoodAndSnack(userName, userFoodType, userURL);
+        //create new second restaurant object using non-default constructor
+        FastFoodAndSnack restaurantAgain = new FastFoodAndSnack(userName, userFoodType, userURL);
 
-                //display using the display method
-                System.out.println("----using display method----");
-                restaurantAgain.display();
-            }
-            
-            i++;
-        }
+        //display using the display method
+        System.out.println("----using display method----");
+        restaurantAgain.display();
+
+        //end session message
         System.out.println("Thank you, session ended.");
     }
 }
