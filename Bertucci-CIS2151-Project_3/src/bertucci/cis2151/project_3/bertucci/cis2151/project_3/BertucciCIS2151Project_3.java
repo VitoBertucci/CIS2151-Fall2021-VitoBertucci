1f43dc13dfadbab3c3d6aef88a0fdc4a5f6f837a
VitoBertucci <vito.rob.bertucci@gmail.com>
2021-09-23 15:28:30 -0400

    v0.0
    Created project 3 and laid out rubric for functions of applications.

---
+++ Bertucci-CIS2151-Project_3/src/bertucci/cis2151/project_3/bertucci/cis2151/project_3/BertucciCIS2151Project_3.java
@@ -0,0 +1,33 @@
+/**
+ * Create an application that calculates the area of various shapes
+ *      -Create abstract class named shape
+ *          -has abstract method that returns double type
+ *      -Create class Circle that inherits shape class and contains:
+ *          -public Circle(double radius)
+ *          -public double getRadius()
+ *          -public void setRadius(double radius)
+ *          -public double getArea()
+ *      -Create class Sqaure that inherits shape class and contains:
+ *          -public Square(double width)
+ *          -public double getWidth()
+ *          -public void setWidth(double width)
+ *          -public double getArea()
+ *      -Create class Rectangle that inherits Square class and contains:
+ *          -public Rectangle(double width, double height)
+ *          -public double getHeight()
+ *          -public void setHeight(double height)
+ *          -public double getArea()
+ *      -Use following formulas to calculate area:
+ *          -Circle: Area = radius2 * pi
+ *          -Square: Area = width2
+ *          -Rectangle: Area = width * height
+ *      -Use console class in chapter 7 to get and validate the user entries, 
+ *       but assume the user enters a valid shape entry
+ */
+
+package bertucci.cis2151.project_3;
+public class BertucciCIS2151Project_3 {
+    public static void main(String[] args) {
+    }
+    
+}
